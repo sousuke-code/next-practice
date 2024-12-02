2d7735bab7aee26a6d084f3e5bc1ce35ce0fe113
sousuke-code <msousuke1@gmail.com>
2024-12-02 23:33:29 +0900

    タスクカードの作成

---
+++ src/app/components/SideMenu/NavList/NavItem/NavItem.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import React from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+interface NavItemProps {
+    label : string;
+    link: string;
+    icon: React.ReactNode;
+}
+const NavItem: React.FC<NavItemProps>= ({
+    label, link, icon
+}) => {
+  const pathname = usePathname()
+
+  return (
+    <Link href={link} className={`flex p-4 items-center w-fuill hover:bg-gray-700 font-medium ${pathname == link ? 'bg-gray-600 border-r-green-500' : ''}`}>
+        <div className="mr-1">{icon}</div>
+        <div>{label}</div>
+    </Link>
+  )
+}
+
+export default NavItem
